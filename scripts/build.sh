611e56c29374d2430e6dd84ff2dab2ac8400e57c
Aleksandra Kurek <aleksandra.kurek@codahead.com>
2018-07-09 11:34:00 +0200

    Clean all folders

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-# transformacja js
-babel --presets react,es2015 js/source/ -d js/build
-# pakowanie js
-browserify js/build/app.js -o bundle.js
-# pakowanie css
-cat css/*/* css/*.css | sed 's/..\/..\/images/images/g' > bundle.css
-# gotowe
-date; echo;
-
